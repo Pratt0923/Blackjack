f260af38f9365ee543728511b046559effd646fd
Alyssa Pratt <snubly1@gmail.com>
2016-05-25 16:19:06 -0400

    game start commit

--- blackjack.rb
+++ blackjack.rb
@@ -6,8 +6,14 @@ require './dealer'
 
 class Deck
 
-  SUITS = [:S, :D, :H, :C]
-  RANKS = 2.upto(10).to_a + [:J, :Q, :K, :A]
+puts "Welcome to Blackjack!"
+puts "How much money do you want to bet?"
+print "$$: "
+input = gets.chomp
+if input != Integer
+  puts "Please enter a valid number"
+  input = gets.chomp
+end
 
   #in blackjack you need to have the dealer get a new deck of cards.
 
@@ -16,3 +22,7 @@ class Deck
 
   #the player will keep drawing cards until they either get to 21 or they
   #go over 21 or whatever
+
+  #after this they will either win or lose
+
+  #then they can play again.
